8ed747eda6994c3354c1167da341590a02ca352d
Althea <altheagene@users.noreply.github.com>
2026-03-03 11:40:58 +0800

    Updated index.js. Added submit route

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//Serve static files from the "public" folder
+app.use(express.static('public'));
+
 //Route for home page
 app.get('/', (req, res) => {
     res.send("Hello World!");
@@ -10,4 +13,15 @@ app.get('/', (req, res) => {
 //Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
+})
+
+app.get('/about', (req, res) => {
+    res.send("About Us");
+})
+
+app.use(express.json()); //Middleware to parse JSON bodies
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
 })
